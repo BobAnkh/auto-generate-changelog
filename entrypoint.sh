3294e4fb0306dd585c9264619443eaa03fc0d258
BobAnkh <bobankhshen@gmail.com>
2020-08-02 22:30:37 +0800

    style(entrypoint): add fileheader

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+###
+ # @Author: BobAnkh
+ # @Github: https://github.com/BobAnkh
+ # @Date: 2020-07-29 23:47:02
+ # @LastEditors: BobAnkh
+ # @LastEditTime: 2020-08-02 22:29:08
+ # @FilePath: /auto-generate-changelog/entrypoint.sh
+ # @Description: 
+ # @Copyright 2020 BobAnkh
+### 
+
 set -e
 
 python -m pip install --upgrade pip setuptools wheel

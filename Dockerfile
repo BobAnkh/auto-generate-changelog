6f807b5e2faef7aa040800dc005cc1b6298a9159
BobAnkh <bobankhshen@gmail.com>
2020-08-10 16:01:27 +0800

    perf(dockerfile): specify git version

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.7.8-slim
 
 RUN apt-get update && \
-    apt-get upgrade -y && \
-    apt-get install -y --no-install-recommends git
+    apt-get install -y --no-install-recommends git=1:2.20.1-2+deb10u3 && \
+    rm -rf /var/lib/apt/lists/*
 
 COPY main.py entrypoint.sh requirements.txt /
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
  # @Author: BobAnkh
  # @Github: https://github.com/BobAnkh
  # @Date: 2020-07-29 23:47:02
- # @LastEditors: BobAnkh
- # @LastEditTime: 2020-08-02 22:33:55
- # @FilePath: /auto-generate-changelog/entrypoint.sh
+ # @LastEditors  : BobAnkh
+ # @LastEditTime : 2020-08-10 15:59:48
+ # @FilePath     : /auto-generate-changelog/entrypoint.sh
  # @Description: Entrypoint of Github Action
  # @Copyright 2020 BobAnkh
 ### 

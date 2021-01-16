ed4a0c85bbcf7bf9b6e7b3354a1b21e4fec1f3a8
BobAnkh <bobankhshen@gmail.com>
2021-01-16 13:52:05 +0800

    feat(mode): add local-dev mode and BRANCH option
    Closes #39, #36

--- main.py
+++ main.py
@@ -4,18 +4,18 @@
 # @Author       : BobAnkh
 # @Github       : https://github.com/BobAnkh
 # @Date         : 2020-08-06 10:48:37
-# @LastEditTime : 2021-01-11 10:36:47
+# @LastEditTime : 2021-01-16 12:36:45
 # @Description  : Main script of Github Action
 # @Copyright 2020 BobAnkh
 
 import argparse
+import base64
 import os
 import re
-import shlex
-import subprocess
 
+import github
 import yaml
-from github import Github
+from tqdm import tqdm
 
 
 def argument_parser():
@@ -38,26 +38,6 @@ def argument_parser():
     parser.add_argument('-t', '--token', help='Github Access Token')
     args = parser.parse_args()
     return args
-
-
-def github_login(ACCESS_TOKEN, REPO_NAME):
-    '''
-    Use PyGithub to login to the repository
-
-    Args:
-        ACCESS_TOKEN (string): github Access Token
-        REPO_NAME (string): repository name
-
-    Returns:
-        github.Repository.Repository: object represents the repo
-
-    References:
-    ----------
-    [1]https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository
-    '''
-    g = Github(ACCESS_TOKEN)
-    repo = g.get_repo(REPO_NAME)
-    return repo
 
 
 def set_local_env(env_name, env_value, prefix='INPUT'):
@@ -119,9 +99,97 @@ def set_env_from_file(file, args, prefix='INPUT'):
                     tmp = args.token
                 else:
                     tmp = input('Please input the value of ' + param + ':')
+            else:
+                tmp = params[param]
         else:
             tmp = params[param]
         set_local_env(param, tmp, prefix)
+
+
+class GithubChangelog:
+    '''
+    Class for data interface of Github
+
+    Use it to get changelog data and file content from Github and write new file content to Github
+    '''
+    def __init__(self, ACCESS_TOKEN, REPO_NAME, PATH, BRANCH, COMMIT_MESSAGE):
+        '''
+        Initial GithubContributors
+
+        Args:
+            ACCESS_TOKEN (str): Personal Access Token for Github
+            REPO_NAME (str): The name of the repository
+            PATH (str): The path to the file
+            BRANCH (str): The branch of the file
+            COMMIT_MESSAGE (str): Commit message you want to use
+        '''
+        self.COMMIT_MESSAGE = COMMIT_MESSAGE
+        self.PATH = PATH
+        self.BRANCH = BRANCH
+        self.SHA = ''
+        self.releases = {}
+        self.changelog = ''
+        # Use PyGithub to login to the repository
+        # References: https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository
+        g = github.Github(ACCESS_TOKEN)
+        self.repo = g.get_repo(REPO_NAME)
+
+    def get_data(self):
+        # get release info
+        releases = self.repo.get_releases()
+        self.releases['Unreleased'] = {'html_url': '', 'body': '', 'created_at': '', 'commit_sha': ''}
+        for release in releases:
+            self.releases[release.tag_name] = {'html_url': release.html_url, 'body': re.sub(r'\r\n', r'\n', release.body), 'created_at': release.created_at}
+        # get tags and commits
+        tags = self.repo.get_tags()
+        for tag in tags:
+            if tag.name in self.releases:
+                self.releases[tag.name]['commit_sha'] = tag.commit.sha
+        release_tags = list(self.releases.keys())[::-1]
+        seq = 0
+        commits = self.repo.get_commits(sha=self.BRANCH).reversed
+        selected_commits = []
+        pbar = tqdm(desc='Commits progress', total=commits.totalCount)
+        for commit in commits:
+            message = commit.commit.message.split('\n\n')
+            message_head = message[0]
+            if message_head[-3:] == '...' and len(message) > 1:
+                if message[1][0:3] == '...':
+                    message_head = re.sub(r'  ', r' ', message_head[:-3] + ' ' + message[1].split('\n')[0][3:])
+            url = commit.html_url
+            pulls = commit.get_pulls()
+            pr_links = []
+            if pulls.totalCount == 0:
+                pass
+            else:
+                for pull in pulls:
+                    pr = f''' ([#{pull.number}]({pull.html_url}))'''
+                    pr_links.append(pr)
+            selected_commits.append({'head': message_head, 'sha': commit.sha, 'url': url, 'pr_links': pr_links})
+            if commit.sha == self.releases[release_tags[seq]]['commit_sha']:
+                self.releases[release_tags[seq]]['commits'] = selected_commits[::-1]
+                selected_commits = []
+                seq = seq + 1
+            pbar.update(1)
+        pbar.close()
+        self.releases[release_tags[seq]]['commits'] = selected_commits[::-1]
+        # get file content
+        contents = self.repo.get_contents(self.PATH, self.BRANCH)
+        self.PATH = contents.path
+        self.SHA = contents.sha
+        base = contents.content
+        base = base.replace('\n', '')
+        self.changelog = base64.b64decode(base).decode('utf-8')
+
+    def read_releases(self):
+        return self.releases
+
+    def write_data(self, changelog):
+        if changelog == self.changelog:
+            pass
+        else:
+            self.repo.update_file(self.PATH, self.COMMIT_MESSAGE, changelog,
+                                  self.SHA, self.BRANCH)
 
 
 def strip_commits(commits, regex):
@@ -129,184 +197,62 @@ def strip_commits(commits, regex):
     Bypass some commits
 
     Args:
-        commits (list): list of commits that each element contains SHA and <head>
-        regex (string): regex expression to match
+        commits (list): list of commit(dict), whose keys are 'head', 'sha', 'url', 'pr_links'
+        regex (string): regex expression to match.
 
     Returns:
-        list: selected commits
-        set: set of scopes
+        dict: selected commits of every scope.
     '''
-    output = []
-    scope_set = []
+    scopes = {}
     for commit in commits:
-        if re.match(regex, commit[1]):
-            scope = re.findall(regex, commit[1])[0]
+        if re.match(regex, commit['head']):
+            scope = re.findall(regex, commit['head'])[0]
             if scope.lower() == 'changelog' and regex == r'^docs[(](.+?)[)]':
                 continue
-            subject = re.sub(regex + r'\s?:\s?', '', commit[1])
-            scope_set.append(scope)
-            commit.append(scope)
-            commit.append(subject)
-            output.append(commit)
-    return output, set(scope_set)
+            subject = re.sub(regex + r'\s?:\s?', '', commit['head'])
+            if scope in scopes:
+                scopes[scope].append({'subject': subject, 'commit': commit})
+            else:
+                scopes[scope] = []
+                scopes[scope].append({'subject': subject, 'commit': commit})
+    return scopes
 
 
-def get_tags():
-    '''
-    Get tags
-
-    Returns:
-        list of string: tag names including 'HEAD'
-    '''
-    command = 'git tag'
-    output_bytes = subprocess.check_output(shlex.split(command),
-                                           stderr=subprocess.STDOUT)
-    output = output_bytes.decode('utf-8')
-    output = output.split('\n')
-    output[-1] = 'HEAD'
-    return output
-
-
-def get_commit_log_between_versions(previous_version, later_version, flag):
-    '''
-    Get the commit log between two versions
-
-    Args:
-        previous_version (string): previous version
-        later_version (string): later version
-        flag (int): set 1 to compare versions
-
-    Returns:
-        list of list: list of commit log(SHA + <head>) between versions
-    '''
-    if flag == 1:
-        log_command = 'git log --pretty=format:"%H %s" ' + previous_version + '...' + later_version
-    else:
-        log_command = 'git log --pretty=format:"%H %s" ' + previous_version
-    output_bytes = subprocess.check_output(shlex.split(log_command),
-                                           stderr=subprocess.STDOUT)
-    output = output_bytes.decode('utf-8')
-    output = output.split('\n')
-    output_list = []
-    for item in output:
-        tmp = []
-        commit_sha = item[0:40]
-        commit_title = item[41:]
-        if commit_title[-3:] == '...':
-            commit_command = f'git log -1 {commit_sha} --pretty=format:"%B"'
-            commit_bytes = subprocess.check_output(shlex.split(commit_command), stderr=subprocess.STDOUT)
-            commit_content = commit_bytes.decode('utf-8')
-            commit_content = commit_content.split('\n\n')
-            if len(commit_content) > 1:
-                if commit_content[1][0:3] == '...':
-                    commit_title = commit_title[:-3] + ' ' + commit_content[1].split('\n')[0][3:]
-        tmp.append(commit_sha)
-        tmp.append(commit_title)
-        output_list.append(tmp)
-    return output_list
-
-
-# def parse_commits(repo):
-#     '''
-#     Parse all commits
-
-#     Args:
-#         repo (github.Repository.Repository): object represents the repo
-
-#     Returns:
-#         list: each element contains a list which is the information of a commit
-
-#     Ref:
-#     commit_info: SHA, html_url, message, pull.number, pull.html_url
-#     '''
-#     parsed_commits = []
-#     commits = repo.get_commits()
-#     for commit in commits:
-#         processed_commit = []
-#         processed_commit.append(commit.sha)
-#         processed_commit.append(commit.html_url)
-#         processed_commit.append(commit.commit.message)
-#         related_pulls = commit.get_pulls()
-#         if related_pulls.totalCount == 0:
-#             processed_commit.append([])
-#         else:
-#             pulls = []
-#             for related_pull in related_pulls:
-#                 pulls.append([related_pull.number, related_pull.html_url])
-#             processed_commit.append(pulls)
-#         parsed_commits.append(processed_commit)
-#     return parsed_commits
-
-
-def parse_releases(repo):
-    '''
-    Parse releases
-
-    Args:
-        repo (github.Repository.Repository): object represents the repo
-
-    Returns:
-        list: each element contains a list which is the information of a release
-
-    Ref:
-    release info: tag_name, html_url, body(description)
-    '''
-    parsed_releases = []
-    releases = repo.get_releases()
-    for release in releases:
-        processed_release = []
-        processed_release.append(release.tag_name)
-        processed_release.append(release.html_url)
-        release_body = re.sub(r'\r\n', r'\n', release.body)
-        processed_release.append(release_body)
-        processed_release.append(release.created_at)
-        parsed_releases.append(processed_release)
-    return parsed_releases
-
-
-def generate_section(commit_list, type_regex, repo):
+def generate_section(release_commits, type_regex):
     '''
     Generate scopes of a section
 
     Args:
-        commit_list (list): commits
+        release_commits (dict): commits of the release
         type_regex (string): regex expression
-        repo (github.Repository.Repository): object represents the repo
 
     Returns:
         string: content of section
     '''
     section = ''
-    sel_commits, scopes = strip_commits(commit_list, type_regex)
+    scopes = strip_commits(release_commits, type_regex)
     for scope in scopes:
         scope_content = f'''- {scope}:\n'''
-        for sel_commit in sel_commits:
-            if sel_commit[2] == scope:
-                commit = repo.get_commit(sel_commit[0])
-                url = commit.html_url
-                pulls = commit.get_pulls()
-                prs = []
-                if pulls.totalCount == 0:
-                    pass
-                else:
-                    for pull in pulls:
-                        pr = f''' ([#{pull.number}]({pull.html_url}))'''
-                        prs.append(pr)
-                scope_content = scope_content + f'''  - {sel_commit[3]} ([{sel_commit[0][0:7]}]({url}))'''
-                for pr_link in prs:
-                    scope_content = scope_content + pr_link
-                scope_content = scope_content + '\n'
+        for sel_commit in scopes[scope]:
+            commit = sel_commit['commit']
+            sha = commit['sha']
+            url = commit['url']
+            subject = sel_commit['subject']
+            pr_links = commit['pr_links']
+            scope_content = scope_content + f'''  - {subject} ([{sha[0:7]}]({url}))'''
+            for pr_link in pr_links:
+                scope_content = scope_content + pr_link
+            scope_content = scope_content + '\n'
         section = section + scope_content + '\n'
     return section
 
 
-def generate_release_body(commit_list, repo, part_name):
+def generate_release_body(release_commits, part_name):
     '''
     Generate release body using part_name_dict and regex_list
 
     Args:
-        commit_list (list): commits
-        repo (github.Repository.Repository): object represents the repo
+        release_commits (dict): commits of the release
         part_name (list): a list of part_name, e.g. feat:Feature
 
     Returns:
@@ -316,76 +262,54 @@ def generate_release_body(commit_list, repo, part_name):
     for part in part_name:
         reg, name = part.split(':')
         regex = r'^'+ reg + r'[(](.+?)[)]'
-        sec = generate_section(commit_list, regex, repo)
+        sec = generate_section(release_commits, regex)
         if sec != '':
             release_body = release_body + '### ' + name + '\n\n' + sec
     return release_body
 
 
-def generate_changelog(repo, parsed_releases, part_name):
+def generate_changelog(releases, part_name):
     '''
     Generate CHANGELOG
 
     Args:
-        repo (github.Repository.Repository): object represents the repo
-        parsed_releases (list): releases' information
+        releases: dict of release data
         part_name (list): a list of part_name, e.g. feat:Feature
 
     Returns:
         string: content of CHANGELOG
     '''
-    tags = get_tags()
     info_list = []
     CHANGELOG = '# CHANGELOG\n\n'
-    for i, _ in enumerate(tags):
+    for release_tag in releases:
         release_info = ''
-        if i == 0:
-            commit_list = get_commit_log_between_versions(tags[i], tags[i], 0)
-        else:
-            commit_list = get_commit_log_between_versions(
-                tags[i - 1], tags[i], 1)
-        if tags[i] == 'HEAD':
+        release_commits = releases[release_tag]['commits']
+        if release_tag == 'Unreleased':
             title = 'Unreleased'
             description = 'Changes unreleased.'
             release_info = f'''## {title}\n\n{description}\n\n'''
         else:
-            for release in parsed_releases:
-                if release[0] == tags[i]:
-                    title = release[0]
-                    url = release[1]
-                    description = release[2]
-                    date = release[3]
-                    release_info = f'''## [{title}]({url}) - {date}\n\n{description}\n\n'''
-                    break
-        release_body = generate_release_body(commit_list, repo, part_name)
-        if release_body == '' and tags[i] == 'HEAD':
-            pass
+            title = release_tag
+            url = releases[release_tag]['html_url']
+            description = releases[release_tag]['body']
+            date = releases[release_tag]['created_at']
+            release_info = f'''## [{title}]({url}) - {date}\n\n{description}\n\n'''
+        release_body = generate_release_body(release_commits, part_name)
+        if release_body == '' and release_tag == 'Unreleased':
+            continue
         else:
             release_info = release_info + release_body
             info_list.append(release_info)
-    for j in reversed(info_list):
+    for j in info_list:
         CHANGELOG = CHANGELOG + j
     CHANGELOG = CHANGELOG + r'\* *This CHANGELOG was automatically generated by [auto-generate-changelog](https://github.com/BobAnkh/auto-generate-changelog)*'
     CHANGELOG = CHANGELOG + '\n'
     return CHANGELOG
 
 
-def write_changelog(repo, changelog, path, commit_message):
-    '''
-    Write contributors list to file if it differs
-
-    Args:
-        repo (github.Repository.Repository): object represents the repo
-        changelog (string): content of changelog
-        path (string): the file to write
-        commit_message (string): commit message
-    '''
-    contents = repo.get_contents(path)
-    repo.update_file(contents.path, commit_message, changelog, contents.sha)
-
-
 def main():
     args = argument_parser()
+    set_env_from_file('.github/workflows/changelog.yml', args)
     if args.mode == 'local':
         set_env_from_file('.github/workflows/changelog.yml', args)
     elif args.mode == 'github':
@@ -397,11 +321,22 @@ def main():
     REPO_NAME = get_inputs('REPO_NAME')
     PATH = get_inputs('PATH')
     COMMIT_MESSAGE = get_inputs('COMMIT_MESSAGE')
-    part_name = get_inputs('TYPE').split(',')
-    repo = github_login(ACCESS_TOKEN, REPO_NAME)
-    parsed_releases = parse_releases(repo)
-    CHANGELOG = generate_changelog(repo, parsed_releases, part_name)
-    write_changelog(repo, CHANGELOG, PATH, COMMIT_MESSAGE)
+    if re.match(r'.*:.*', PATH):
+        BRANCH = re.sub(r':.*', '', PATH)
+        PATH = re.sub(r'.*:', '', PATH)
+    else:
+        BRANCH = github.GithubObject.NotSet
+    part_name = re.split(r'\s?,\s?', get_inputs('TYPE'))
+    changelog = GithubChangelog(ACCESS_TOKEN, REPO_NAME, PATH, BRANCH, COMMIT_MESSAGE)
+    changelog.get_data()
+
+    CHANGELOG = generate_changelog(changelog.read_releases(), part_name)
+
+    if args.mode == 'local':
+        with open(args.output, 'w', encoding='utf-8') as f:
+            f.write(CHANGELOG)
+    else:
+        changelog.write_data(CHANGELOG)
 
 
 if __name__ == '__main__':
